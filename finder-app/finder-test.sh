cefac8e4b79b217a4d8938cd1b87c49f509c1efc
C5338602 <oleksii.krasnovskyi@sap.com>
2022-12-26 20:43:00 +0200

    modify finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -9,9 +9,9 @@ NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
 username=$(cat /etc/finder-app/conf/username.txt)
-DIR=$(dirname $0)
+assignment=`cat /etc/finder-app/conf/assignment.txt`
 
-if [ $# -lt 2 ]
+if [ $# -lt 3 ]
 then
 	echo "Using default value ${WRITESTR} for string to write"
 	if [ $# -lt 1 ]
@@ -32,12 +32,10 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
 rm -rf "${WRITEDIR}"
 
-# create $WRITEDIR if not assignment4
-assignment=`cat /etc/finder-app/conf/assignment.txt`
+
 
 if [ $assignment != 'assignment1' ]
 then
-	
 	mkdir -p "$WRITEDIR"
 
 	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
@@ -50,27 +48,24 @@ then
 		exit 1
 	fi
 fi
-#echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
+
 
 for i in $( seq 1 $NUMFILES)
 do
-	DIR/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	/usr/bin/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(DIR/finder.sh "$WRITEDIR" "$WRITESTR")
-echo OUTPUTSTRING > /tmp/assignment4-result.txt
+OUTPUTSTRING=$(/usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
 
-#remove temporary directories
+# remove temporary directories
 rm -rf /tmp/aeld-data
 
 set +e
-echo ${OUTPUTSTRING} | grep "${MATCHSTR}" 
+echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
 	echo "success"
 	exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found ${OUTPUTSTRING}"
+	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
 	exit 1
 fi
